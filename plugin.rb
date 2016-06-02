f6df42ed05808603122116f05ebce1345133d4ec
Ken Cooper <ken@coopercode.net>
2016-06-02 13:08:12 -0700

    Debug

--- plugin.rb
+++ plugin.rb
@@ -10,13 +10,13 @@ after_initialize do
     Rails.logger.info("Topic created, maybe notifying, enabled: #{SiteSetting.new_topic_notification_enabled} notifyinguser: #{SiteSetting.new_topic_notifying_user} topic category: #{topic.category.name}")
     # TODO need to detect category is proper, need to map category to category name.
 
-      if SiteSetting.new_topic_notification_enabled && SiteSetting.new_topic_notifying_user.present? then # && topic.category.name == SiteSetting.new_topic_notification_category.strip then
+      if SiteSetting.new_topic_notification_enabled && SiteSetting.new_topic_notifying_user.present? && topic.category.name == SiteSetting.new_topic_notification_category.strip then
         user_notifying_new_post = User.where(username_lower: SiteSetting.new_topic_notifying_user.strip).first
         Rails.logger.info("User notifying: #{user_notifying_new_post}")
         if user_notifying_new_post then
           last_topic = nil
-          Topic.where(category_id: topic.category_id).recent(2) do | existing_topic |
-            Rails.logger.info("Checking if topic #{existing_topic_id} matches #{topic.id}")
+          Topic.where(category_id: topic.category.id).recent(2) do | existing_topic |
+            Rails.logger.info("Checking if topic #{topic.category.id} matches #{topic.id}")
             if existing_topic.id != topic.id
               last_topic = existing_topic
             end
