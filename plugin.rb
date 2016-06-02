ccc538107e2aa9fced83d5f0be7b62dfc9012457
Ken Cooper <ken@coopercode.net>
2016-06-01 17:55:41 -0700

    More bug fixes

--- plugin.rb
+++ plugin.rb
@@ -6,42 +6,37 @@
 
 after_initialize do
   DiscourseEvent.on(:topic_created) do | topic, options, user |
-    Rails.logger.debug("Topic created, maybe notifying, enabled: #{SiteSetting.new_topic_notification_enabled} notifyinguser: #{SiteSetting.new_topic_notifying_user}")
-      if SiteSetting.new_topic_notification_enabled && SiteSetting.new_topic_notifying_user.present? then
-        user_notifying_new_post = User.where(username_lower: SiteSetting.new_topic_notifying_user).first
+    Rails.logger.debug("Topic created, maybe notifying, enabled: #{SiteSetting.new_topic_notification_enabled} notifyinguser: #{SiteSetting.new_topic_notifying_user} topic category: #{topic.category.name}")
+    # TODO need to detect category is proper, need to map category to category name.
+
+      if SiteSetting.new_topic_notification_enabled && SiteSetting.new_topic_notifying_user.present?&& topic.category.name == SiteSetting.new_topic_notification_category.strip then
+        user_notifying_new_post = User.where(username_lower: SiteSetting.new_topic_notifying_user.strip).first
         Rails.logger.debug("User notifying: #{user_notifying_new_post}")
         if user_notifying_new_post then
-          new_topic_category_id = Category.where(name: SiteSetting.new_topic_notification_category).first.id
-          Rails.logger.debug("Topic category id: #{new_topic_category_id}")
-          if new_topic_category_id then
-            last_topic = nil
-            # I'm sure there's a magic ruby way to get the last of two elements or nil...
-            Topic.where(category_id: new_topic_category_id).recent(2) do | existing_topic |
-              Rails.logger.debug("Checking if topic #{existing_topic_id} matches #{topic.id}")
-              if existing_topic.id != topic.id
-                last_topic = existing_topic
-              end
+          last_topic = nil
+          Topic.where(category_id: topic.category_id).recent(2) do | existing_topic |
+            Rails.logger.debug("Checking if topic #{existing_topic_id} matches #{topic.id}")
+            if existing_topic.id != topic.id
+              last_topic = existing_topic
             end
-            if last_topic then
-              Rails.logger.debug("Last topic: #{last_topic.title}")
-              topic_embed = TopicEmbed.where(topic_id: topic.id).first
-              if topic_embed then
-                Rails.logger.debug("Topic embed: #{topic_embed}")
-                embed_url = topic_embed.embed_url
-                raw = SiteSetting.new_topic_raw_body
-                raw = raw.sub('{blog_new_topic_url}', embed_url)
-                raw = raw.sub('{title}', topic.title)
-                raw = raw.sub('{discourse_new_topic_url}', topic.url)
-                creator = PostCreator.new(user_notifying_new_post, topic_id: last_topic.id, raw: raw)
-                creator.create
-              else
-                Rails.logger.debug("Could not find topic embed for topic id: #{topic.id}")
-              end
+          end
+          if last_topic then
+            Rails.logger.debug("Last topic: #{last_topic.title}")
+            topic_embed = TopicEmbed.where(topic_id: topic.id).first
+            if topic_embed then
+              Rails.logger.debug("Topic embed: #{topic_embed}")
+              embed_url = topic_embed.embed_url
+              raw = SiteSetting.new_topic_raw_body
+              raw = raw.sub('{blog_new_topic_url}', embed_url)
+              raw = raw.sub('{title}', topic.title)
+              raw = raw.sub('{discourse_new_topic_url}', topic.url)
+              creator = PostCreator.new(user_notifying_new_post, topic_id: last_topic.id, raw: raw)
+              creator.create
             else
-              Rails.logger.debug("Could not find previous topic in category: #{SiteSetting.new_topic_notification_category}")
+              Rails.logger.debug("Could not find topic embed for topic id: #{topic.id}")
             end
           else
-            Rails.logger.debug("Could not find matching category for topics to notify against: #{SiteSetting.new_topic_notification_category}")
+            Rails.logger.debug("Could not find previous topic in category: #{SiteSetting.new_topic_notification_category}")
           end
         else
           Rails.logger.debug("Could not find matching user to assign as notifier: #{SiteSetting.new_topic_notifying_user}")
